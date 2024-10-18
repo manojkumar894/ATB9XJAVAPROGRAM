4f1bf7d8819c62ba874bc7690d54d7225d5a55cc
manojkumar894 <manojkmr.kumar894@gmail.com>
2024-10-18 13:21:14 +0530

    Lab001 created

---
+++ src/OCT/ex_02102024/Lab001.java
@@ -0,0 +1,13 @@
+package OCT.ex_02102024;
+
+public class Lab001 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+    }
+
+
+
+}
