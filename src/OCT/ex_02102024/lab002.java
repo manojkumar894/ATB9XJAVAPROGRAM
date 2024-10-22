3d54ba940a26cb100b521ecfebf018091b04b68c
manojkumar894 <manojkmr.kumar894@gmail.com>
2024-10-22 19:34:16 +0530

    sub created

---
+++ src/OCT/ex_02102024/lab002.java
@@ -0,0 +1,12 @@
+package OCT.ex_02102024;
+
+public class lab002 {
+    public static void main(String[] args) {
+        int a= 10;
+        int b = 25;
+        int sub = 0;
+        sub =  a- b;
+        System.out.print(sub);
+    }
+
+}
